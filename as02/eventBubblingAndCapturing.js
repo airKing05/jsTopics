35fc05ddb7a74684008668e6118564581a05e318
airKing05 <anil050797@gmail.com>
2022-06-13 03:36:22 +0530

    polyfill-bind, event bibbling, capturing, stopPropagation

---
+++ as02/eventBubblingAndCapturing.js
@@ -0,0 +1,35 @@
+console.log("this even bubbling")
+document.querySelector('#grandParent')
+    .addEventListener('click', () => {
+        console.log("grand parent clicked")
+    }, true); //capturing, //capturing, //bubbling
+
+// by default useCapturing value is false/ bubbling
+
+document.querySelector('#parent')
+    .addEventListener('click', (e) => {
+        console.log("parent clicked")
+        // to stop the event in bubbling
+        e.stopPropagation();
+    }, true) // bubbling, //bubbling, //capturing
+
+document.querySelector('#child')
+    .addEventListener('click', (e) => {
+        console.log("child clicked")
+         // to stop the event in capturing
+        e.stopPropagation()
+    }, true) // capturing, //bubbling, //capturing
+
+// top me capturing hai to phle capturing cycle complete hogi git bubbling wala callback call hoga
+// if top hi capturing ho or last dono bubbling hoto, phle capturung, second wale ko check krega ydi capturing nahi hai to aage wale jake usko check krega, isme bhi ydi false hai to phle child hi call hoga yha se event bubbling ki cycle start ho jayegi
+// third case me parent>child>grandParent
+// 4th case me child>parent>grandParent
+
+
+// stop propagation cases
+// is jgh pr propagation laga huaa hai vha par aake ruk jayega
+// for bubbling
+// 1st case => child>parent
+
+// for capturing
+// 1st case => child>parent
---
+++ as02/polyfillBind.js
@@ -0,0 +1,38 @@
+let nameObj = {
+    firstname: "anilraj",
+    lastname: "meena"
+};
+function getFullName(city, state, msg){
+    console.log(`name is ${this.firstname} ${this.lastname} from ${city}, ${state} and he ${msg}`)
+}
+let finalName = getFullName.bind(nameObj, 'Bundi', 'Raj');
+finalName('he is good boy by noraml bind')
+
+// polyfill for bind method
+
+// every function can access bind method 
+// so for it we use prototype finalName2()
+Function.prototype.myBind = function(...args){
+    // this -> getFullName, we myBind called 'this' will point to the 'fetFullName'
+    // store this into variable
+    let obj = this;
+    // bind method return any other function, so
+    // args2 passes the data from
+    return function(...args2){ 
+        // if we called finalName2() our getFullName() called
+       // getFullName()
+       // get argument like args[0] pointing the name
+
+       //for the second parameter
+       // remove first argument from entire array's of argument, remaining inside the params
+       params = args.slice(1)
+       //obj.call(args[0])
+        obj.apply(args[0], [...params, ...args2])  // concat bot args and args2
+
+       console.log(args[0], params)
+    }
+
+}
+                // this.polyfill(object)
+let finalName2 = getFullName.myBind(nameObj, 'Bundi', 'Raj');
+finalName2('is bad boy');
